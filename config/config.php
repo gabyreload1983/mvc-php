b9f225faddf5ea442202fdf479ff98c43b92ffbe
Gabriel Godoy <gabyreload@gmail.com>
2023-10-11 22:02:49 -0300

    Create DB.php class

--- config/config.php
+++ config/config.php
@@ -1,7 +1,15 @@
 <?php
 
 define('DEBUG', true);
+
+define('DB_NAME', 'freeskills');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', '127.0.0.1');
+
 define('DEFAULT_CONTROLLER','Home');
 define('DEFAULT_LAYOUT', 'default');
+
 define('PROOT', '/');
+
 define('SITE_TITLE', 'Freeskills MVC Framework');
